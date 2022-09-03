e3bef296aea1f8bc3d2b060407d2a239a43828e6
Hubert Olszewski <holszewski@consorg.pl>
2022-09-03 23:51:59 +0200

    Added trigger on enter key

--- src/components/LogScreen.tsx
+++ src/components/LogScreen.tsx
@@ -1,5 +1,6 @@
 import { Input, Typography, styled } from "@mui/material"
 import { Stack } from "@mui/system"
+import { keyboardKey } from "@testing-library/user-event";
 import { FC } from "react";
 import { BasicStyleButton } from "./BasicButton";
 
@@ -15,11 +16,15 @@ interface ILogScreenProps{
 }
 
 export const LogScreen: FC<ILogScreenProps> = ({onClickBtn, onChangeInput, isDisabled}) => {
+    const handleKeyDown = (e: keyboardKey) => {
+        e.keyCode === 13 && onClickBtn();
+    }
+
     return (
     <Stack alignItems={'center'} top='26%' position={'relative'}>
         <Stack sx={{background: '#276350'}} alignItems={'center'} padding={'50px'} borderRadius={'10%'}>
             <Typography variant="h3" color={'white'}>Your Username</Typography>
-            <Input sx={{width: '240px', background: '#ffffff', margin: '50px'}} type="text" onChange={(event) => onChangeInput(event)} required/>
+            <Input sx={{width: '240px', background: '#ffffff', margin: '50px'}} type="text" onKeyDown={handleKeyDown} onChange={(event) => onChangeInput(event)} required/>
             <SendButton disabled={isDisabled} onClick={onClickBtn} sx={{padding: '5px', marginBottom: '50px'}} >Submit</SendButton>
         </Stack>
     </Stack>
