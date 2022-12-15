ed55bd6db809fbc1df80d27ea4d02ab6619ba70e
Hubert Olszewski <holszewski@consorg.pl>
2022-12-15 23:26:56 +0100

    Added IUser interface

--- src/onBoard/Views/GameView.tsx
+++ src/onBoard/Views/GameView.tsx
@@ -2,20 +2,22 @@ import { Socket } from "socket.io-client";
 import { ChatView } from "./ChatView";
 import { FC } from "react";
 import { BoardView } from "./BoardView";
+import { IUser } from '../../App';
 
 interface IGameViewProps{
     socket: Socket;
     gameId: string | undefined;
     userName: string;
-    opponentUserName: string;
+    opponentUsers: IUser[];
+    currentUser: IUser;
 }
 
-export const GameView: FC<IGameViewProps> = ({socket, gameId, userName, opponentUserName}) => {
+export const GameView: FC<IGameViewProps> = ({socket, gameId, userName, opponentUsers, currentUser}) => {
     
     return (
         <div>
-            <BoardView />
-            <ChatView socket={socket} gameId={gameId} userName={userName} opponentUserName={opponentUserName}></ChatView>
+            <BoardView socket={socket}/>
+            <ChatView socket={socket} gameId={gameId} userName={userName} opponentUsers={opponentUsers} currentUser={currentUser}/>
         </div>
     );
 }
