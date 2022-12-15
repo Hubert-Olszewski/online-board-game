cc70c818d3b1bd445518482e71a48df0f2d92ecd
Hubert Olszewski <holszewski@consorg.pl>
2022-12-15 23:27:06 +0100

    ControlPanelView

---
+++ src/onBoard/Views/ControlPanelView.tsx
@@ -0,0 +1,36 @@
+import { FC, useState } from 'react';
+import { Socket } from 'socket.io-client';
+import { IUser } from '../../App';
+import { StyledButton } from '../../components/BasicButton';
+
+interface IControlPanelViewProps {
+    socket: Socket;
+}
+
+const rollDice = () => Math.floor(Math.random() * 6) + 1;
+
+export const ControlPanelView: FC<IControlPanelViewProps> = ({socket}) => {
+    const [numberDrawn, setNumberDrawn] = useState<number>(0);
+    const [user, setUser] = useState<IUser>();
+
+    socket.on('getUserProps', (user: IUser) => setUser(user));
+
+    return (
+        <div className="control-panel">
+            <div className="panel-container">
+                <div className="left-panel">
+                    <div className="current-player">
+                        <div>{user ? user.userName : undefined}</div>
+                        <div>{user ? user.money : undefined}$</div>
+                    </div>
+                    <div className="drawn-number">
+                        <div>Number drawn:</div>
+                        <div>{numberDrawn}</div>
+                    </div>
+                </div>
+                <div className="right-panel"></div>
+                <StyledButton className="roll-dice" onClick={() => setNumberDrawn(rollDice() + rollDice())}>Draw a number</StyledButton>
+            </div>
+        </div>
+    );
+}
