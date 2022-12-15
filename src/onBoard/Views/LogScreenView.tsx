84692fa8353f2be1a7702a073773ce525cfa8192
Hubert Olszewski <holszewski@consorg.pl>
2022-12-15 23:24:32 +0100

    Rename and path changed

--- src/onBoard/Views/LogScreenView.tsx
+++ src/onBoard/Views/LogScreenView.tsx
@@ -1,13 +1,8 @@
-import { Input, Typography, styled } from "@mui/material"
-import { Stack } from "@mui/system"
+import { Input, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
 import { keyboardKey } from "@testing-library/user-event";
 import { FC } from "react";
-import { BasicStyleButton } from "./BasicButton";
-
-export const SendButton = styled(BasicStyleButton)({
-    textAlign: 'center',
-    display: 'flex'
-});
+import { StyledButton } from "../../components/BasicButton";
 
 interface ILogScreenProps{
     onClickBtn: () => void;
@@ -15,7 +10,7 @@ interface ILogScreenProps{
     isDisabled: boolean;
 }
 
-export const LogScreen: FC<ILogScreenProps> = ({onClickBtn, onChangeInput, isDisabled}) => {
+export const LogScreenView: FC<ILogScreenProps> = ({onClickBtn, onChangeInput, isDisabled}) => {
     const handleKeyDown = (e: keyboardKey) => {
         e.keyCode === 13 && onClickBtn();
     }
@@ -25,7 +20,7 @@ export const LogScreen: FC<ILogScreenProps> = ({onClickBtn, onChangeInput, isDis
         <Stack sx={{background: '#276350'}} alignItems={'center'} padding={'50px'} borderRadius={'10%'}>
             <Typography variant="h3" color={'white'}>Your Username</Typography>
             <Input sx={{width: '240px', background: '#ffffff', margin: '50px'}} type="text" onKeyDown={handleKeyDown} onChange={(event) => onChangeInput(event)} required/>
-            <SendButton disabled={isDisabled} onClick={onClickBtn} sx={{padding: '5px', marginBottom: '50px'}} >Submit</SendButton>
+            <StyledButton disabled={isDisabled} onClick={onClickBtn} sx={{padding: '5px', marginBottom: '50px'}}>Submit</StyledButton>
         </Stack>
     </Stack>
     )
